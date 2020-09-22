7a86e53b16d8b21435daed4f619388110a602221
Jose Alanya <jose.alanya@gmail.com>
2020-09-22 17:27:43 -0500

    FEAT: Ingredient: Ingredients - more useEffect() and useCallback()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,21 +7,9 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-update-2aade.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    });
-  }, []);
+  const filteredIngredientHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  },[]);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-2aade.firebaseio.com/ingredients.json', {
@@ -50,7 +38,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientHandler}/>
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
